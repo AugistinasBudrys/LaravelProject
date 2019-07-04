b2ab822a9e16603632868acdc9cbecc258193211
Augustinas Budrys <augustinas.budrys@stud.vgtu.lt>
2019-07-04 10:13:08 +0300

    registracija, login, userlist

---
+++ resources/views/partials/alerts.blade.php
@@ -0,0 +1,10 @@
+    @if(session('success'))
+    <div class="alert alert-primary" role="alert">
+        {{session('success')}}
+    </div>
+    @endif
+    @if(session('warning'))
+    <div class="alert alert-danger" role="alert">
+        {{session('warning')}}
+    </div>
+    @endif
